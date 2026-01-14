6f790035feec6c33128ceb450f3f332bab829cef
Ashish <ashishmurkar23@gmail.com>
2026-01-14 17:06:49 +0530

    add flip-computeandstorage-prj in project.tf file

--- project.tf
+++ project.tf
@@ -3,3 +3,9 @@ resource "google_project" "flip-network-prj" {
   project_id = "flip-network-prj-001"
   folder_id = google_folder.flip_fldr.name
 }
+
+resource "google_project" "flip-computeandstorage-prj" {
+  name       = "flip-computeandstorage-prj"
+  project_id = "flip-compute-prj-001"
+  folder_id  = google_folder.flip_fldr.name
+}

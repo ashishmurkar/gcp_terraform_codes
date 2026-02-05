9d81bd70418d8fbba8a1fcdfb41246dac682903c
Ashish <ashishmurkar23@gmail.com>
2026-02-05 22:05:10 +0530

    remove app subnet

--- flip/subnet.tf
+++
@@ -1,9 +0,0 @@
-resource "google_compute_subnetwork" "app_sub" {
-  name          = "app-sub"
-  ip_cidr_range = "198.168.2.0/29"
-  region        = "us-east1"
-  network       = google_compute_network.flip-vpc.id
-  project       = "flip-network-prj-001"
-
-  private_ip_google_access = true
-}

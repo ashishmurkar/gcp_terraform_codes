a96e52238d4dcdedac90addc9b77c9b0278d9258
Ashish <ashishmurkar23@gmail.com>
2026-02-07 19:52:15 +0530

    update

--- flip/vpc.tf
+++ flip/vpc.tf
@@ -1,16 +1,16 @@
-# resource "google_compute_network" "flip-vpc" {
+ #resource "google_compute_network" "flip-vpc" {
   # name                    = "flip-vpc"
-  # project                 = local.flip_network_prj
-  # auto_create_subnetworks = false
-  # routing_mode            = "REGIONAL"
-# }
+   #project                 = local.flip_network_prj
+   #auto_create_subnetworks = false
+   #routing_mode            = "REGIONAL"
+ #}
 
 
 
 
 
 module "flip_vpc" {
-  source = "./module"
+  source = "./modules"
   vpc_name                 = "flip-vpc"
   project_id               = google_project.flip-network-prj.project_id
   auto_create_subnetworks  = false
